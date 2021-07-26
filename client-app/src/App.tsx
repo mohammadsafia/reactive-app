851e896218965a4292cb92117f258ab5739c511f
Mohammad Safia <msafia@terkwaz.com>
2021-07-26 17:47:32 +0300

    Create react app, install axios, semantic-ui

---
+++ client-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+import './App.css';
+
+function App() {
+    const [activities, setActivities] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/Activities').then(res => {
+            setActivities(res.data);
+        });
+    }, []);
+    return (
+        <div>
+            <Header as="h2" icon="users" content="Reactivites"/>
+            <List>
+                { activities.map((activity: any) => <List.Item key={ activity.id }>{ activity.title }</List.Item>) }
+            </List>
+        </div>
+    );
+}
+
+export default App;
